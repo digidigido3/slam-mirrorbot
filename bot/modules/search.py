4af817ab027255b2545a58e09e919d05186f2ffe
Juan <72490350+jusidama18@users.noreply.github.com>
2021-06-18 21:37:17 +0700

    Update search.py

--- bot/modules/search.py
+++ bot/modules/search.py
@@ -302,15 +302,15 @@ RESULT_STR_ALL = (
 )
 
 torrents_dict = {
-    '1337x': {'source': f"{TORRENT_API_URL_1}/api/1337x/", 'result_str': RESULT_STR_1337},
-    'piratebay': {'source': f"{TORRENT_API_URL_1}/api/piratebay/", 'result_str': RESULT_STR_PIRATEBAY},
-    'tgx': {'source': f"{TORRENT_API_URL_1}/api/tgx/", 'result_str': RESULT_STR_TGX},
-    'yts': {'source': f"{TORRENT_API_URL_2}/yts?query=", 'result_str': RESULT_STR_YTS},
-    'eztv': {'source': f"{TORRENT_API_URL_1}/api/eztv/", 'result_str': RESULT_STR_EZTV},
-    'torlock': {'source': f"{TORRENT_API_URL_1}/api/torlock/", 'result_str': RESULT_STR_TORLOCK},
-    'rarbg': {'source': f"{TORRENT_API_URL_1}/api/rarbg/", 'result_str': RESULT_STR_RARBG},
-    'nyaasi': {'source': f"{TORRENT_API_URL_1}/api/rarbg/", 'result_str': RESULT_STR_RARBG}, # For Alternative Search For Nyaa.si
-    'ts': {'source': f"{TORRENT_API_URL_1}/api/all/", 'result_str': RESULT_STR_ALL}
+    ['1337x']: {'source': f"{TORRENT_API_URL_1}/api/1337x/", 'result_str': RESULT_STR_1337},
+    ['piratebay', 'pb']: {'source': f"{TORRENT_API_URL_1}/api/piratebay/", 'result_str': RESULT_STR_PIRATEBAY},
+    ['tgx']: {'source': f"{TORRENT_API_URL_1}/api/tgx/", 'result_str': RESULT_STR_TGX},
+    ['yts']: {'source': f"{TORRENT_API_URL_2}/yts?query=", 'result_str': RESULT_STR_YTS},
+    ['eztv']: {'source': f"{TORRENT_API_URL_1}/api/eztv/", 'result_str': RESULT_STR_EZTV},
+    ['torlock']: {'source': f"{TORRENT_API_URL_1}/api/torlock/", 'result_str': RESULT_STR_TORLOCK},
+    ['rarbg']: {'source': f"{TORRENT_API_URL_1}/api/rarbg/", 'result_str': RESULT_STR_RARBG},
+    ['nyaasi']: {'source': f"{TORRENT_API_URL_1}/api/rarbg/", 'result_str': RESULT_STR_RARBG}, # For Alternative Search For Nyaa.si
+    ['ts']: {'source': f"{TORRENT_API_URL_1}/api/all/", 'result_str': RESULT_STR_ALL}
 }
 
 torrent_handlers = []

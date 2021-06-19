414d37d984c567f7a3988e5c13d0bf3a01fda4e8
Juan <72490350+jusidama18@users.noreply.github.com>
2021-06-19 09:18:52 +0700

    Update config.py

--- bot/modules/config.py
+++ bot/modules/config.py
@@ -3,7 +3,7 @@
 
 from pyrogram import filters, types, emoji
 from bot.helper.telegram_helper.bot_commands import BotCommands
-from bot import app, OWNER_ID
+from bot import app, OWNER_ID, IMAGE_URL
 from bot.helper import get_text, check_heroku
 from bot.modules import ALL_MODULES
 from bot import *
@@ -53,7 +53,7 @@ __header__='📕 **Page** **{}**\n\n'
 
 @app.on_message(filters.command(BotCommands.ConfigMenuCommand) & filters.user(OWNER_ID))
 async def config_menu(_, message):
-    await message.reply(
+    await message.reply_photo(photo=IMAGE_URL, 
         f"**Hello {message.from_user.mention}**,\n\n**If you want to add or set Variable in Heroku use** `/setvar`\n\n**If you want to delete Variable in Heroku use `/delvar`**\n\n**If you want to Update Bot with Upstream Repo use `/{BotCommands.UpdateCommand}`**\n\n**WARNING! Very Recommended to do this command in private since it's contain bot info.**\n\n**Here's This is Slam-MirrorBot Current Configs**\n\n\n",
         reply_markup=types.InlineKeyboardMarkup(
             [[types.InlineKeyboardButton("VAR NAME", url="https://github.com/breakdowns/slam-mirrorbot#setting-up-config-file"), types.InlineKeyboardButton(f"{emoji.CROSS_MARK}", callback_data='docs_end'), types.InlineKeyboardButton(f"BOT CONFIG", callback_data='docs_1')]]
